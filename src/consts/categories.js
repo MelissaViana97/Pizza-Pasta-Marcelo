d9e35f96aafa2f9f8ff572b4cf26aee4e5b80f07
MelissaViana97 <70685997+MelissaViana97@users.noreply.github.com>
2022-10-13 23:54:27 -0600

    Add files via upload

---
+++ src/consts/categories.js
@@ -0,0 +1,7 @@
+const categories = [
+    {id: '1', name: 'pizza', image: require('../assets/catergories/pizza.png')},
+    {id: '2', name: 'Hamburgueza', image: require('../assets/catergories/pasta.png')},
+   
+  ];
+  
+  export default categories;
---
+++ src/consts/foods.js
@@ -0,0 +1,65 @@
+
+
+const foods=[
+    {
+        id:'1',
+        name:'Pizza de Carne',
+        ingredientes:'Carne Pizza',
+        precio: '7.50',
+        image: require('../assets/carnePizza.png'),
+    },
+    {
+        id:'2',
+        name:'Pizza de Queso',
+        ingredientes:'Queso Pizza',
+        precio: '8.50',
+        image: require('../assets/quesoPizza.png'),
+    },
+    {
+        id:'3',
+        name:'Pizza Suprema',
+        ingredientes:'Suprema Pizza',
+        precio: '9.50',
+        image: require('../assets/suprema-pizza.png'),
+    },
+    {
+        id:'4',
+        name:'Pizza de Peperoni',
+        ingredientes:'Peperoni Pizza',
+        precio: '6.50',
+        image: require('../assets/peperoni-pizza.png'),
+    },
+         //PASTAS
+    {
+        id:'5',
+        name:'Pasta Carne',
+        ingredientes:'Carne Pasta',
+        precio: '5.50',
+        image: require('../assets/pasta-carne.png'),
+    },
+    {
+        id:'6',
+        name:'Pata Queso',
+        ingredientes:'Queso Pastas',
+        precio: '6.00',
+        image: require('../assets/pasta-queso.png'),
+    },
+    {
+        id:'7',
+        name:'Pata Salsa',
+        ingredientes:'Salsa Pastas',
+        precio: '6.00',
+        image: require('../assets/pasta-salsa.png'),
+    },
+    {
+        id:'8',
+        name:'Pata Vegetariana',
+        ingredientes:'Vegetal Pastas',
+        precio: '4.00',
+        image: require('../assets/pasta-vegetal.png'),
+    },
+
+
+
+];
+export default foods;
